a9203457fc21aa809fa513d46915c933462462c1
Mahmoud <mahmoudaboulata01@gmail.com>
2026-06-14 22:28:01 +0300

    fetching fred data

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,5 +1,6 @@
 import wbgapi as wb
 import pandas as pd
+import requests
 
 country = "EGY"
 years = range(2011, 2024)
@@ -15,15 +16,48 @@ indicators = {
 def fetch_world_bank():
     frames = []
     for code, name in indicators.items():
-        df = wb.data.DataFrame(code, COUNTRY, time = YEARS).T
+        df = wb.data.DataFrame(code, country, time = years).T
         df.columns = [name]
         frames.append(df)
 
-compined = pd.concat(frames, axis = 1)
-compined.index.name = "year"
-compined.to_csv("data/raw/world_bank.csv")
-print("SUCCESFULLY!, World bank data is saved :)")
+    combined = pd.concat(frames, axis=1)
+    combined.index.name = "year"
+    combined.to_csv("data/raw/world_bank.csv")
+    print("SUCCESFULLY!, World bank data is saved :)")
+
+def fetch_fred():
+    FRED_API_KEY = "fb11bef5215ff9d8c629a93df88d7742" 
+
+    series = {
+        "XRNCUSEGA618NRUG": "egy_usd_exchange_rate",
+        "FPCPITOTLZGEGY":   "egy_inflation_fred",
+    }
+
+    frames = []
+    for code, name in series.items():
+        url = (
+            f"https://api.stlouisfed.org/fred/series/observations"
+            f"?series_id={code}&api_key={FRED_API_KEY}&file_type=json"
+            f"&observation_start=2011-01-01&observation_end=2023-12-31"
+        )
+        response = requests.get(url)
+        data = response.json()
+        
+
+        df = pd.DataFrame(data["observations"])[["date", "value"]]
+        df["date"] = pd.to_datetime(df["date"])
+        df["value"] = pd.to_numeric(df["value"], errors="coerce")
+        df["year"] = df["date"].dt.year
+        df = df.groupby("year")["value"].mean().reset_index()
+        df = df.rename(columns={"value": name}).set_index("year")
+        frames.append(df)
+
+    combined = pd.concat(frames, axis=1)
+    combined.index.name = "year"
+    combined.to_csv("data/raw/fred_data.csv")
+    print("SUCCESFULLY!, FRED data saved :)")
 
 if __name__ == "__main__":
     fetch_world_bank()
+    fetch_fred()
 
